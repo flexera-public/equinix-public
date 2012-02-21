da8e3da807a80ddb1df36576b940c5f0988eec77
Dev Server <edwin@rightscale.com>
2012-02-21 16:51:35 +0000

    updating package deps for rhel 6

--- cookbooks/app_tomcat/attributes/tomcat.rb
+++ cookbooks/app_tomcat/attributes/tomcat.rb
@@ -23,12 +23,22 @@ case platform
 when "ubuntu", "debian"
   set[:db_mysql][:socket] = "/var/run/mysqld/mysqld.sock"
 when "centos","fedora","suse","redhat"
-  set[:tomcat][:package_dependencies] = ["eclipse-ecj",\
-                                         "tomcat6",\
-                                         "tomcat6-admin-webapps",\
-                                         "tomcat6-webapps",\
-                                         "tomcat-native",\
-                                         "mysql-connector-java"]
+  if node[:platform_version] =~ /5/
+    set[:tomcat][:package_dependencies] = ["eclipse-ecj",\
+                                           "tomcat6",\
+                                           "tomcat6-admin-webapps",\
+                                           "tomcat6-webapps",\
+                                           "tomcat-native",\
+                                           "mysql-connector-java"]
+  else
+    set[:tomcat][:package_dependencies] = ["ecj",\
+                                           "tomcat6",\
+                                           "tomcat6-admin-webapps",\
+                                           "tomcat6-webapps",\
+                                           "tomcat-native",\
+                                           "mysql-connector-java"]
+  end
+
   set[:tomcat][:module_dependencies] = [ "proxy", "proxy_http" ]
   set_unless[:tomcat][:app_user] = "tomcat"
   set[:db_mysql][:socket] = "/var/lib/mysql/mysql.sock"
